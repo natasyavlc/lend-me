b65d35bc1394533f7b368db88fd079694ac11055
natasyavlc <nathtasyavlc12@gmail.com>
2024-10-17 21:42:07 +0700

    fix : change folder name

--- lib/constant/textTheme.dart
+++ lib/constant/textTheme.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:lend_me/globalTheme/colorTheme.dart';
+import 'package:lend_me/constant/colorTheme.dart';
 
 class TextThemeStyle {
   static TextStyle headerLarge = TextStyle(
